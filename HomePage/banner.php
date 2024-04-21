be5840346e70ad5cf54bbb1a2e7ce83bae9e81ef
kaushik raj <124484486+kaushik-raj@users.noreply.github.com>
2024-04-21 17:25:25 +0530

    Delete HomePage directory

--- HomePage/banner.php
+++
@@ -1,4 +0,0 @@
-<section class="cta">
-            <h1>Enroll For Our Various Online Courses<br>Anywhere From The World</h1>
-            <a href="./contact.php" class=" hero-btn" style="text-decoration: none; color: #fff; border-radius: 25px;">CONTACT US</a>
-        </section>
